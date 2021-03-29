bfd3c68b48ea7e7a8b85931d4de5fc6df2b99c7b
S Anand <root.node@gmail.com>
2021-03-29 10:28:43 +0530

    ENH: Initial commit

---
+++ README.md
@@ -0,0 +1,201 @@
+# UIFactory
+
+UIFactory is a simple HTML component library.
+
+- **It's small**. <1.5KB minified, gzipped.
+- **Nothing new to learn**. No shadow DOM. No virtual DOM. Just regular HTML, CSS and JS.
+
+## Installation
+
+Using [npm](https://www.npmjs.com/get-npm):
+
+```bash
+npm install uifactory lodash
+```
+
+To include it in your script, use
+
+```html
+<script src="node_modules/lodash/lodash.min.js"></script>
+<script src="node_modules/uifactory/uifactory.js"></script>
+```
+
+## Components are HTML elements
+
+For example, you can create a `<g-repeat value="8">★</g-repeat>` that component that repeats the star (★) 8 times.
+
+![8 stars](docs/g-repeat-8-star.png)
+
+## ... defined as lodash templates
+
+Any `<template component="x-xx">` defines a new `<x-xx>` element.
+
+For example, this creates the `g-repeat` component we saw above:
+
+```html
+<template component="g-repeat" value="30">
+  <% for (var j=0; j < +value; j++) { %>
+    <%= this.innerHTML %>
+  <% } %>
+</template>
+```
+
+The `component="g-repeat"` attribute defines a `<g-repeat>` component. You **MUST** have a hypen in the component name.
+
+Wherever `<g-repeat></g-repeat>` is placed, the [lodash template](https://lodash.com/docs/#template) is rendered inside it.
+
+- `<%= ... %>` renders JavaScript code.
+- `<% ... %>` runs JavaScript code without rendering.
+
+So, `<g-repeat value="8">★</g-repeat>` renders:
+
+![8 stars](docs/g-repeat-8-star.png).
+
+## Access attributes as variables
+
+Any attributes in your `<template>` is available as a variable in JavaScript. For example, `<template value="30">` in the example below becomes the JavaScript variable `value`:
+
+```html
+<template component="g-repeat" value="30">
+  <% for (var j=0; j < +value; j++) { %>
+    <%= this.innerHTML %>
+  <% } %>
+</template>
+```
+
+When using the component, e.g. `<g-repeat value="8"></g-repeat>`, `value` comes `"8"`.
+
+If you add a component attribute, e.g. `<g-repeat color="red">`, *without* adding it to the template, you can't access `color` as a variable. (But you can access it via [`$target.getAttribute('color')`](#access-component-as-target).)
+
+You can also get or set these as attributes as properties. For example:
+
+```js
+let el = document.querySelector('g-repeat')   // Find the first <g-repeat>
+console.log(el.value)                         // Prints the value=".."
+el.value = 10                                 // Changes value="..." and re-renders
+```
+
+![Access and change properties](docs/g-repeat-properties.gif)
+
+Notes:
+
+- All attributes on the `<template>` are converted to variables, *except* `component=`. It defines the component tag name
+- Attribute names with hyphens are converted to *camelCase*. For example, `<template font-size="8">` converts to variable `fontSize`.
+- Attributes become *string* variables. You need to convert it to a number using `+value`.
+- When you change the attribute or property, the component is *re-rendered*.
+
+## Access `<template>` as `this`
+
+`this` points to the template element (e.g. `<template component="g-repeat">`). For example, you can access contents of your `<template>` via `this.innerHTML`.
+
+- `this` is a copy of component template as a DOM element.
+- `this.innerHTML` has whatever was in your template.
+- `this.querySelectorAll('*')` and similar expressions can access parts of the template
+
+## Access component as `$target`
+
+`$target` points to the element you add (e.g. `<g-repeat>`). For example, this adds a click event listener to each `g-repeat` component.
+
+```html
+<template component="g-repeat" value="30">
+  <% for (var j=0; j < +value; j++) { %>
+    <%= this.innerHTML %>
+  <% } %>
+  <%
+    $target.addEventListener('click', console.log, { once: true })
+  %>
+</template>
+```
+
+![Access $target element](docs/g-repeat-click-event.gif)
+
+You can access component attributes, e.g. `<g-repeat color="red">`, via `$target.getAttribute('color')`.
+
+Remember: `this` is the `<template>`. `$target` is the component, e.g. `<g-repeat>`
+
+<!-- TODO: Explain why once: true -->
+
+## Style components
+
+Use regular CSS to style the components. The `<template>` is rendered directly inside the component (not a shadow DOM). So you can style the contents directly.
+
+For example, this adds a yellow background to `<g-repeat>` if it has `value="8"`:
+
+```html
+<template component="g-repeat" value="30">
+  <style>
+    g-repeat[value="8"] { background-color: yellow; }
+  </style>
+  <% for (var j=0; j < +value; j++) { %>
+    <%= this.innerHTML %>
+  <% } %>
+</template>
+```
+
+![Yellow background applied to g-repeat](docs/g-repeat-8-star-yellow.png)
+
+UIFactory copies all `<style>`s and `<link rel="stylesheet">`s into the document's HEAD, and runs them only once (even if you use the component multiple times.)
+
+## Add events
+
+Use a `<script>` tag to add events or define your component's behavior. For example:
+
+```html
+<template component="g-repeat" value="30">
+  <% for (var j=0; j < +value; j++) { %>
+    <%= this.innerHTML %>
+  <% } %>
+  <script>
+    $('body').on('click', 'g-repeat', console.log)
+  </script>
+</template>
+```
+
+![Add events via event delegation](docs/g-repeat-click-delegate.gif)
+
+This `<script>` will be executed *before the component is rendered*. So use [event delegation](https://davidwalsh.name/event-delegate), e.g. using [jQuery](https://api.jquery.com/on/) or [delegate](https://www.npmjs.com/package/delegate).
+
+Whenever a component is rendered, it fires a `render` event that you can listen for. For example, this prints every component rendered.
+
+```js
+$('body').on('render', function (e) {
+  console.log('We just rendered', e.target)
+})
+```
+
+## Register components
+
+You can register components via JavaScript like this:
+
+```js
+uifactory.register({
+  name: 'g-repeat',
+  template: '<% for (var j=0; j<+value; j++) { %><%= this.innerHTML %><% } %>',
+  options: [
+    { name: "value", value: "30" }
+  ]
+})
+```
+
+`uifactory.register()` accepts an object with these keys:
+
+- `name`: component name, e.g. `"g-repeat"`
+- `template`: component contents as a [lodash template](#-defined-as-lodash-templates)
+- `options`: OPTIONAL: a list of [attributes](#access-attributes-as-variables) as `{name, value}` objects
+- `window`: OPTIONAL: the [Window](https://developer.mozilla.org/en-US/docs/Web/API/Window) on which to register the component. Used to define components on other windows or IFrames
+
+## Load components
+
+You can register components from a JSON file by calling `uifactory.register(url)`. For example,
+
+```js
+uifactory.register('g-repeat.json`)
+```
+
+... fetches `g-repeat.json` and calls `uifactory.register(...contents of the JSON...)`.
+
+## Support
+
+Tested in Chrome 74+, Firefox 66+, Edge 18+, and Safari 11+. IE is not supported.
+
+You can raise issues and feature requests at <https://github.com/gramener/uifactory/issues>.
---
+++ uifactory.js
@@ -0,0 +1,198 @@
+/* eslint-env es6 */
+
+(function(window) {
+  // From https://github.com/lodash/lodash/blob/master/unescape.js
+  const htmlUnescapes = {
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&#39;': "'"
+  }
+  const reEscapedHtml = /&(?:amp|lt|gt|quot|#(0+)?39);/g
+  const reHasEscapedHtml = RegExp(reEscapedHtml.source)
+  function unescape(string) {
+    return (string && reHasEscapedHtml.test(string))
+      ? string.replace(reEscapedHtml, (entity) => (htmlUnescapes[entity] || "'"))
+      : (string || '')
+  }
+
+  // Register a single component
+  function _registerComponent(config) {
+    // The custom element is defined on this window. This need not be the global window.
+    // For an iframe, you can use registerComponent({window: iframe.contentWindow}).
+    const _window = config.window || window
+
+    // convert attributes (e.g. font-size) to camelCase (e.g. fontSize)
+    const camelize = s => s.replace(/-./g, x => x.toUpperCase()[1])
+
+    // Each window has its own component registry. "this" picks the registry of the current window
+    // If a component is already registered, don't re-register.
+    // TODO: Should this be an error or a warning?
+    if (_window.customElements.get(config.name))
+      return console.trace(`Can't redefine component ${config.name} on ${_window}`)
+
+    // Parse the template into DOM element
+    const parser = new DOMParser()
+    const dom = parser.parseFromString(config.template, 'text/html')
+
+    // Add the styles and scripts into the target document under <head> and <body>
+    // target is "head" or "body" -- the element where we add it into the document
+    loadExtract('head', dom.documentElement.querySelectorAll('link[rel="stylesheet"], style'))
+    loadExtract('body', dom.documentElement.querySelectorAll('script'))
+    function loadExtract(target, els, _start = 0) {
+      // Loop through elements starting from the start index
+      for (let index = _start; index < els.length; index++) {
+        let el = els[index]
+        // Copy the element into the target document with attributes
+        const clone = _window.document.createElement(el.tagName)
+        for (let attr of el.attributes)
+          clone.setAttribute(attr.name, attr.value)
+        clone.innerHTML = el.innerHTML
+        // If this is a <script src="...">, then load the remaining extracts AFTER it's loaded
+        // WHY? If I use <script src="jquery"> and then <script>$(...)</script>
+        //    the 2nd script should wait for the 1st script to load.
+        // Why not just...
+        //    Use clone.async = false? Doesn't work
+        //    Add all scripts to a documentFragment and add it at one shot? Doesn't work
+        let externalScript = el.tagName.toLowerCase() == 'script' && el.hasAttribute('src')
+        if (externalScript)
+          clone.onload = clone.onerror = () => loadExtract(target, els, index + 1)
+        _window.document[target].appendChild(clone)
+        // If this is a <script src="...">, we've scheduled the next loadExtract.
+        // So stop looping
+        if (externalScript)
+          break
+      }
+    }
+    // Remove extracted styles and scripts from template
+    for (let el of dom.documentElement.querySelectorAll('link[rel="stylesheet"], style, script'))
+      el.remove()
+    // Convert the rest of the template into a Lodash template
+    const template = _.template(unescape(dom.body.innerHTML))
+    // The {name: ...} from the options list become the observable attrs
+    const options = config.options || []
+    const attrs = options.map(option => option.name)
+
+    // Create the custom HTML element
+    class UIFactory extends _window.HTMLElement {
+      connectedCallback() {
+        // Called when the component is created. "this" is the created HTMLElement.
+
+        // Expose the defined attributes as properties.
+        // <g-component attr="val"> exponses el.attr == "val"
+        attrs.forEach(attr => {
+          Object.defineProperty(this, attr, {
+            get: function () {
+              return this.getAttribute(attr)
+            },
+            set: function (val) {
+              this.setAttribute(attr, val)
+            }
+          })
+        })
+
+        // this.__obj holds the object passed to the template.
+        // Initialize this.__obj with default values.
+        let obj = this.__obj = {}
+        options.forEach(({ name, value }) => obj[name] = value)
+        // Element attributes are available as variables. Convert kebab-case to camelCase
+        for (var i = 0, len = this.attributes.length; i < len; i++)
+          obj[camelize(this.attributes[i].name)] = this.attributes[i].value
+        // template can access the component at $target
+        obj.$target = this
+        // template can access to the original children via "this"
+        this.__originalNode = this.cloneNode(true)
+
+        // this.render() re-renders the object based on current options.
+        this.render()
+      }
+
+      render(config) {
+        // "this" is the HTMLElement. Apply the lodash template
+        this.innerHTML = template.call(this.__originalNode, Object.assign(this.__obj, config))
+        // Generate a render event on this component for initialization
+        this.dispatchEvent(new CustomEvent('render', { bubbles: true }))
+      }
+
+      // The list of attributes to watch for changes on is based on the keys of
+      // config.options, When any of these change, attributeChangedCallback is called.
+      static get observedAttributes() {
+        return attrs
+      }
+
+      // When any attribute changes, update this.__obj and re-render
+      attributeChangedCallback(name, oldValue, newValue) {
+        // If the component is not initialized, don't render it
+        // If it's intialized, re-render
+        if (this.__obj) {
+          this.__obj[name] = newValue
+          this.render()
+        }
+      }
+    }
+
+    // Use customElements from current window.
+    // To use a different window, use createComponent.call(your_window, component)
+    _window.customElements.define(config.name, UIFactory)
+  }
+
+  // If called via <script src="components.js" component="g-component, ...">, load each component
+  let fetchComponent
+  if (document.currentScript) {
+    let base = document.currentScript.src.substring(0, document.currentScript.src.lastIndexOf('/'))
+    fetchComponent = component => {
+      fetch(`${base}/component/${component}.json`)
+        .then(response => response.json())
+        .then(config => registerComponent(config))
+        .catch(error => console.error(error))
+    }
+    let components = (document.currentScript.getAttribute('component') || '').trim()
+    if (components)
+      components.split(/[,+ ]+/g).forEach(fetchComponent)
+  }
+
+  // Any <template component="..."> becomes a component
+  document.querySelectorAll('template[component]').forEach(component => {
+    // A template like <template component="comp" attr="val">
+    // has componentname = "comp" and config.options = {attr: {value: "val", type: "text"}}
+    let componentname, options = []
+    for (let attr of component.attributes)
+      if (attr.name == 'component')
+        componentname = attr.value.toLowerCase()
+      else
+        options.push({ name: attr.name, type: 'text', value: attr.value })
+
+    // Create the custom component on the current window
+    _registerComponent({
+      name: componentname,
+      // If <template> tag is used unescape the HTML. It'll come through as &lt;tag-name&gt;
+      // But if <script> tag is used, no need to unescape it.
+      // Currently, we don't allow a <script> tag, so always unescape it.
+      template: unescape(component.innerHTML),
+      options: options
+    })
+  })
+
+  // uifactory.register({ name: 'g-component', template: '<%= 1 + 2 %>', window: window })
+  //    creates a <g-component> with the lodash template
+  // uifactory.register('g-comp1', 'g-comp2')
+  //    loads these components from ../component/g-comp1.json, etc
+  // uifactory.register.call({...})
+  //    registers component in specified window
+  // AVOID ARROW => FUNCTIONS. We need to preserve "this". "this" defaults to the current window.
+  // But uifactory.register.call(iframe_window, {...}) will register in the iframe_window.
+  // TODO: Allow registering a custom DOM element
+  this.uifactory = {
+    register: function (...configs) {
+      // TODO: handle this
+      configs.forEach(config => {
+        if (typeof config == 'object')
+          _registerComponent(config)
+        else if (fetchComponent && typeof config == 'string')
+          fetchComponent(config)
+      })
+    }
+  }
+
+})(this)
